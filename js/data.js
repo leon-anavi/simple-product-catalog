64af9c4b05ca2de3e2b870be189461380531a106
Leon Anavi <leon@anavi.org>
2016-04-06 02:02:49 +0300

    Generate categories on the home page
    Read categories from data.js and load them on
    the home page.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ js/data.js
@@ -0,0 +1,6 @@
+categories = [
+    { name: "Category1", image: "grid4.jpg" },
+    { name: "Category2", image: "grid5.jpg" },
+    { name: "Category3", image: "grid6.jpg" },
+    { name: "Category4", image: "grid7.jpg" }
+  ];
