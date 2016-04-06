f7706fd924697e50eaff9cf024d57cf1c5a7aa48
Leon Anavi <leon@anavi.org>
2016-04-06 23:34:44 +0300

    Dynamically load categories and products
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ js/catalog.js
@@ -0,0 +1,85 @@
+function getUrlParameter(index) {
+  var url = decodeURIComponent(window.location.search.substring(1));
+  var parameters = url.split('&');
+
+  for (var iter = 0; iter < parameters.length; iter++) {
+    var parameter = parameters[iter].split('=');
+
+    if (parameter[0] === index) {
+      return parameter[1] === undefined ? true : parameter[1];
+    }
+  }
+  return '';
+};
+
+function retrieveCategory() {
+  var category = getUrlParameter('c');
+  //if category is not set use a default value
+  if ('' == category) {
+    category = 0;
+  }
+  //Update category label
+  $('#labelCategory').html(categories[category].name.toUpperCase());
+  $('#labelCategory').attr('href', 'products.html?c=' + category);
+  return category;
+}
+
+function createItem(name, image, link) {
+  var html = '<div class="col-md-4 grid-stn simpleCart_shelfItem">';
+  html += '<div class="img">';
+  html += '<a href="';
+  html += link;
+  html += '"><img src="images/';
+  html += image;
+  html += '" alt="/" class="img-responsive gri-wid"></a>';
+  html += '</div>';
+  html += '<div class="info">';
+  html += '<div class="pull-left styl-hdn">';
+  html += '<a href="';
+  html += link;
+  html += '"><h3>';
+  html += name;
+  html += '</h3></a>';
+  html += '</div>';
+  html += '<div class="clearfix"></div>';
+  html += '</div>';
+  html += '</div>';
+
+  return html;
+}
+
+function createCategories(onlyMenu) {
+  for (var menuItem=0; menuItem<categories.length; menuItem++) {
+    var link = 'products.html?c=' + menuItem;
+    //Append categories to the main menu in the header
+    var menuText = '<li><a href="';
+    menuText += link;
+    menuText += '">';
+    menuText += categories[menuItem].name;
+    menuText += '</a></li>';
+    $("#menu").append(menuText);
+
+    if (true !== onlyMenu) {
+      //Apend category name and image to the list-style
+      $("#categoriesList").append(createItem(
+        categories[menuItem].name, categories[menuItem].image, link));
+    }
+  }
+
+  $("#categoriesList").append('<div class="clearfix"></div>');
+}
+
+function createProducts(category) {
+  var selectedProducts = products[category];
+  if (undefined === selectedProducts) {
+    return;
+  }
+  for (var iter=0; iter<selectedProducts.length; iter++) {
+    var link = 'single.html?c=' + category + '&p=' + iter;
+    //Apend category name and image to the list-style
+    $("#productsList").append(createItem(
+      selectedProducts[iter].name, selectedProducts[iter].thumbnail, link));
+  }
+
+  $("#productsList").append('<div class="clearfix"></div>');
+}
--- js/data.js
+++ js/data.js
@@ -4,3 +4,93 @@ categories = [
     { name: "Category3", image: "grid6.jpg" },
     { name: "Category4", image: "grid7.jpg" }
   ];
+
+products = [
+  // category
+  [
+    // product
+    { name: "Test",
+      description: "",
+      details: "",
+      thumbnail: "grid4.jpg",
+      images: ["show.jpg", "show1.jpg", "show2.jpg", "show3.jpg" ],
+      downloads: []
+    },
+    // product
+    { name: "Test2",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: [],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Suspendisse consectetur",
+      description: "",
+      details: "",
+      thumbnail: "grid4.jpg",
+      images: [],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: [],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Suspendisse consectetur",
+      description: "",
+      details: "",
+      thumbnail: "grid3.jpg",
+      images: [],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: [],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid5.jpg",
+      images: [],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid7.jpg",
+      images: [],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Test",
+      description: "",
+      details: "",
+      thumbnail: "grid4.jpg",
+      images: [],
+      downloads: []
+    }
+  ],
+]
